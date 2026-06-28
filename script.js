db4ed4538dfb4b7b98040b5ba5eeb60f2957c4b1
elakkiya010607 <aloneponnuelakki007@gmail.com>
2026-06-28 18:36:57 +0530

    add js

---
+++ script.js
@@ -0,0 +1,10 @@
+function join(){
+
+let name=document.getElementById("name").value;
+
+let std=document.getElementById("standard").value;
+
+document.getElementById("result").innerHTML=
+"Welcome "+name+"<br>You selected "+std;
+
+}
